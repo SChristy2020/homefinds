69d7eebb2e9bde60e243e52ec3f91c9033b58667
Christy <qsa8647332@gmail.com>
2026-03-05 20:15:21 -0500

    feat: GitHub Pages Setting

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
+/*
+ * @Author: Christy qsa8647332@gmail.com
+ * @Date: 2026-03-05 21:21:30
+ * @LastEditors: Christy qsa8647332@gmail.com
+ * @LastEditTime: 2026-03-05 19:29:15
+ * @FilePath: \homefinds\vite.config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
+  base: '/homefinds/',
   plugins: [vue()],
   resolve: {
     alias: {
