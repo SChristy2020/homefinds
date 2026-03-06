5e21536590f899d18967ec83258fb203ca5c6f9c
Christy <qsa8647332@gmail.com>
2026-03-05 21:27:00 -0500

    fix: GitHub Pages Setting

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@
  * @Author: Christy qsa8647332@gmail.com
  * @Date: 2026-03-05 21:21:30
  * @LastEditors: Christy qsa8647332@gmail.com
- * @LastEditTime: 2026-03-05 19:29:15
+ * @LastEditTime: 2026-03-05 21:25:56
  * @FilePath: \homefinds\vite.config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -11,7 +11,7 @@ import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  base: '/homefinds/',
+  base: '/',
   plugins: [vue()],
   resolve: {
     alias: {
