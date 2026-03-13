908b90715ee128489aebf0eccfdfc9af0ac64f8b
Christy <qsa8647332@gmail.com>
2026-03-13 14:34:51 -0400

    fix: GitHub Pages重新整理就會404
    運作方式：
    
    本地開發：Vite dev server 會攔截所有路由，回傳 index.html → 正常
    GitHub Pages：是純靜態伺服器，/homefinds/rent 找不到對應檔案 → 404

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -6,7 +6,7 @@
  * @FilePath: \homefinds\src\router\index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import ShopView from '@/views/ShopView.vue'
 import RentView from '@/views/RentView.vue'
 import OrdersView from '@/views/OrdersView.vue'
@@ -20,6 +20,6 @@ const routes = [
 ]
 
 export default createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL), //正確引導到 homefinds/
+  history: createWebHashHistory(import.meta.env.BASE_URL), //正確引導到 homefinds/
   routes,
 })
